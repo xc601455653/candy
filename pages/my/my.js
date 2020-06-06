b20912f4fa72179a77117f01d9f649b9ef786cd2
xucan <15071482539@163.com>
2020-06-06 14:00:38 +0800

    部分代码

--- pages/my/my.js
+++ pages/my/my.js
@@ -5,14 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: null,
+    canIUse: wx.canIUse("button.open-type.getUserInfo")
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({ userInfo: res.userInfo });
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
@@ -62,5 +74,24 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  bindGetUserInfo: function (e) {
+    if (e.detail.userInfo) {
+      //用户按了允许授权按钮
+      this.setData({userInfo: e.detail.userInfo});
+    } else {
+      //用户按了拒绝按钮
+      wx.showModal({
+        title: "警告",
+        content: "您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!",
+        showCancel: false,
+        confirmText: "返回授权",
+        success: function (res) {
+          if (res.confirm) {
+            console.log("用户点击了“返回授权”");
+          }
+        }
+      });
+    }
+  },
 })
